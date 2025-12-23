fa7a00273cfe2cca1c347aa34052cba3d42d5683
Siddhartha Pathak <sidx404@gmail.com>
2025-12-23 21:20:02 +0545

    fja

--- main.py
+++ main.py
@@ -28,3 +28,7 @@ def api_test():
         "message": "API connection successful",
         "success": True
     }
+@app.get("/print-name/{name}")
+def print_name(name: str):
+    print(f"User name is: {name}")
+    return {"name": name}
